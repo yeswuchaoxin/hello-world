8c8401322d5311f0fd2cd902819d7160e052a9b6
zhongguoqiaobusi <43949085+zhongguoqiaobusi@users.noreply.github.com>
2019-11-26 15:43:30 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h> 
 #include <curl/curl.h>
 int main(void)
+{
 CURL *curl;
 CURLcode res;
+  
 curl=curl_easy_init();
 if(curl){
 curl_easy_setopt(curl, CURLOPT-URL,"http://myip.ipip.net/");
